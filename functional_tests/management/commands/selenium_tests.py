6c15f57ad272070a504f908693665b921f8c7b75
Steven Skoczen <steven@quantumimagery.com>
2011-11-18 17:01:18 -0800

    massive refactoring for parallelizable django tests. Setup/teardown for n stacks appears to be working.

--- functional_tests/management/commands/selenium_tests.py
+++ functional_tests/management/commands/selenium_tests.py
@@ -1,79 +1,225 @@
+from multiprocessing import Pool
+import subprocess
+import os
+from os.path import abspath, join
+import time 
+
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
-import subprocess
-from os.path import abspath, join
-import time 
+# from django.db import connection
+from django.db import connections, DEFAULT_DB_ALIAS
 from functional_tests.helpers import print_exception
+from django.template.loader import render_to_string
+from django import VERSION
+
+try:
+    import djcelery
+    CELERY_ENABLED = True
+except:
+    CELERY_ENABLED = False
+
+def setup_selenium_stack(worker_index, verbosity, options):
+    files = []
+    silent_output = file('/dev/null', 'a+')
+    files.append(silent_output)
+    test_runner_settings = options.get('settings', None)
+    if verbosity > 1:
+        outputs={}
+    else:
+        outputs = {
+            "stderr":silent_output,
+            "stdout":silent_output,
+        }
+    
+    manage_folder = ""
+    try:
+        import manage
+        manage_folder = abspath(join(manage.__file__, ".."))
+    except:
+        pass
+
+    test_settings = getattr(settings,"FUNCTIONAL_TEST_SERVER_SETTINGS", test_runner_settings)
+    if test_settings:
+        test_settings = "sel_worker_%s" % worker_index
+    
+    test_server_settings = "--settings=%s" % test_settings
+    print test_server_settings
+
+
+    lots_of_options_dict = {
+        've_path':              getattr(settings,"VIRTUALENV_PATH", abspath(join(abspath(__file__),"..","..","..","..","..",".."))),
+        'http_port':            getattr(settings,"BASE_FUNCTIONAL_TESTS_LIVE_SERVER_PORT",8099)+worker_index, 
+        'test_server_settings': test_server_settings,
+        'lib_path' :            abspath(join(abspath(__file__), "..", "selenium_lib")), 
+        "selenium_port":        getattr(settings,"BASE_FUNCTIONAL_TESTS_SELENIUM_PORT",4444)+worker_index,
+        'file_uploader_port':   getattr(settings,"BASE_FUNCTIONAL_TESTS_FILE_UPLOADER_PORT", 8199)+worker_index,
+        'project_root':         getattr(settings,"PROJECT_ROOT", manage_folder),
+        'worker_index':         worker_index,
+        'test_runner_settings': test_runner_settings,
+    }
+    settings_file = file('%s/sel_worker_%s.py' % (manage_folder, worker_index), 'w+')
+    if test_settings != "":
+        settings_file.write(render_to_string('functional_tests/settings.py.txt', lots_of_options_dict))
+    settings_file.close()
+
+    setup_databases(**lots_of_options_dict)
+
+    sel_command =  "java -jar %(lib_path)s/selenium-server.jar -timeout 30 -port %(selenium_port)s -userExtensions %(lib_path)s/user-extensions.js" % lots_of_options_dict
+    gun_command =  "%(ve_path)s/bin/python manage.py run_gunicorn -w 2 -b 0.0.0.0:%(http_port)s %(test_server_settings)s" % lots_of_options_dict
+    cel_command =  "%(ve_path)s/bin/python manage.py celeryd %(test_server_settings)s" % lots_of_options_dict
+    file_uploader_command = "%(ve_path)s/bin/python -m SimpleHTTPServer %(file_uploader_port)s" % lots_of_options_dict
+    print gun_command
+    
+    subprocesses = []    
+    # subprocesses.append(subprocess.Popen(sync_command, shell=True, **outputs ))
+    subprocesses.append(subprocess.Popen(sel_command, shell=True, **outputs ))
+    subprocesses.append(subprocess.Popen(file_uploader_command, shell=True, cwd=join(settings.PROJECT_ROOT,"templates/functional_test_uploads"), **outputs ))
+    # time.sleep(10)
+    subprocesses.append(subprocess.Popen(gun_command, shell=True, **outputs ))
+    if CELERY_ENABLED:
+        subprocesses.append(subprocess.Popen(cel_command, shell=True, **outputs ))
+    return subprocesses, silent_output
+
+def teardown_selenium_stack(worker_index, subprocesses, silent_output):
+    try:
+        silent_output.close()    
+    except:
+        print_exception()
+        pass
+
+    for s in subprocesses:
+        try:
+            s.kill()
+        except: 
+            pass
+
+    manage_folder = ""
+    try:
+        import manage
+        manage_folder = abspath(join(manage.__file__, ".."))
+    except:
+        pass
+    try:
+        print '%s/sel_worker_%s.py' % (manage_folder, worker_index)
+        os.remove('%s/sel_worker_%s.py' % (manage_folder, worker_index))
+        os.remove('%s/sel_worker_%s.pyc' % (manage_folder, worker_index))
+    except Exception, e:
+        print e
+        pass
+
+    print "Stopping..."
+    time.sleep(8)
+
+
+
+def setup_databases(**kwargs):
+    if VERSION[0] == 1:
+        if VERSION[1] == 2 and VERSION[2] < 4:
+            return setup_databases_12(**kwargs)
+        elif VERSION[2] >= 4 or VERSION[1] == 3:
+            return setup_databases_13(**kwargs)
+
+    raise Exception('Unsupported Django Version: %s' % (str(VERSION)))
+
+def setup_databases_12(**kwargs):
+    # Taken from django.test.simple
+    old_names = []
+    mirrors = []
+
+    worker_index = kwargs.get('worker_index', None)
+    for alias in connections:
+        connection = connections[alias]
+        database_name = 'test_%d_%s' % (worker_index, connection.settings_dict['NAME'])
+        connection.settings_dict['TEST_NAME'] = database_name
+        if connection.settings_dict['TEST_MIRROR']:
+            mirrors.append((alias, connection))
+            mirror_alias = connection.settings_dict['TEST_MIRROR']
+            connections._connections[alias] = connections[mirror_alias]
+        else:
+            old_names.append((connection, connection.settings_dict['NAME']))
+            connection.creation.create_test_db(verbosity=0, autoclobber=True)
+    return old_names, mirrors
+
+def setup_databases_13(**kwargs):
+    # Taken from django.test.simple
+    from django.test.simple import dependency_ordered
+
+    mirrored_aliases = {}
+    test_databases = {}
+    dependencies = {}
+
+    worker_index = kwargs.get('worker_index', None)
+    for alias in connections:
+        connection = connections[alias]
+        database_name = 'test_%d_%s' % (worker_index, connection.settings_dict['NAME'])
+        connection.settings_dict['TEST_NAME'] = database_name
+
+        item = test_databases.setdefault(
+            connection.creation.test_db_signature(),
+            (connection.settings_dict['NAME'], [])
+        )
+        item[1].append(alias)
+        if alias != DEFAULT_DB_ALIAS:
+            dependencies[alias] = connection.settings_dict.get('TEST_DEPENDENCIES', [DEFAULT_DB_ALIAS])
+
+    old_names = []
+    mirrors = []
+    for signature, (db_name, aliases) in dependency_ordered(test_databases.items(), dependencies):
+        connection = connections[aliases[0]]
+        old_names.append((connection, db_name, True))
+        test_db_name = connection.creation.create_test_db(verbosity=0, autoclobber=True)
+        for alias in aliases[1:]:
+            connection = connections[alias]
+            if db_name:
+                old_names.append((connection, db_name, False))
+                connection.settings_dict['NAME'] = test_db_name
+            else:
+                old_names.append((connection, db_name, True))
+                connection.creation.create_test_db(verbosity=0, autoclobber=True)
+
+    for alias, mirror_alias in mirrored_aliases.items():
+        mirrors.append((alias, connections[alias].settings_dict['NAME']))
+        connections[alias].settings_dict['NAME'] = connections[mirror_alias].settings_dict['NAME']
+
+        return old_names, mirrors
 
 class Command(BaseCommand):
     help = "Run the selenium tests."
     __test__ = False
 
     def handle(self, *args, **options):
-        silent_output = file('/dev/null', 'a+')
+                
         verbosity = int(options.get('verbosity', 1))
         base_port = int(options.get('base_port', 0))
+        num_processes = int(options.get('functional_parallel_stacks', 1))
 
-        if verbosity > 1:
-            outputs={}
-        else:
-            outputs = {
-                "stderr":silent_output,
-                "stdout":silent_output,
-            }
 
-        test_settings = getattr(settings,"FUNCTIONAL_TEST_SERVER_SETTINGS","")
-        if test_settings != "":
-            test_settings = "--settings=%s" % test_settings
+        self._pool = Pool(processes=num_processes)
+        subprocess_stacks = []
+        output_files = []
+        results = []
+        print "Starting stacks..."
+        for base_port in range(0,num_processes):
+            results.append(self._pool.apply_async(setup_selenium_stack, [base_port, verbosity, options]))
 
-        lots_of_options_dict = {
-            've_path':              getattr(settings,"VIRTUALENV_PATH", abspath(join(abspath(__file__),"..","..","..","..","..",".."))),
-            'http_port':            getattr(settings,"LIVE_SERVER_PORT",8099)+base_port, 
-            'test_server_settings': test_settings,
-            'lib_path' :            abspath(join(abspath(__file__), "..", "selenium_lib")), 
-            "selenium_port":        getattr(settings,"SELENIUM_PORT",4444)+base_port,
-            'file_uploader_port':   getattr(settings,"FUNCTIONAL_TEST_FILE_UPLOADER_PORT", 8199)+base_port
-        }
-
-        sel_command =  "java -jar %(lib_path)s/selenium-server.jar -timeout 30 -port %(selenium_port)s -userExtensions %(lib_path)s/user-extensions.js" % lots_of_options_dict
-        gun_command =  "%(ve_path)s/bin/python manage.py run_gunicorn -w 2 -b 0.0.0.0:%(http_port)s %(test_server_settings)s" % lots_of_options_dict
-        cel_command =  "%(ve_path)s/bin/python manage.py celeryd %(test_server_settings)s" % lots_of_options_dict
-        file_uploader_command = "%(ve_path)s/bin/python -m SimpleHTTPServer %(file_uploader_port)s" % lots_of_options_dict
+        for result in results:
+            s,o = result.get()
+            subprocess_stacks.append(s)
+            output_files.append(o)
         
-        selenium_subprocess = subprocess.Popen(sel_command,shell=True,              **outputs )
-        file_uploader_subprocess = subprocess.Popen(file_uploader_command,shell=True, cwd=join(settings.PROJECT_ROOT,"templates/functional_test_uploads"), **outputs )
-        time.sleep(10)
-        gunicorn_subprocess = subprocess.Popen(gun_command,shell=True,              **outputs )
-        celery_subprocess = subprocess.Popen(cel_command,shell=True,                **outputs )
-
         try:
             call_command('test', "--with-selenium", *args, **options )
-            silent_output.close()    
-        except:
-            print_exception()
-            pass
-        
-        try:
-            gunicorn_subprocess.kill()
         except:
             pass
 
-        try:
-            selenium_subprocess.kill()
-        except:
-            pass
+        self._pool.close()
+        self._pool.join()
+        self._pool = Pool(processes=num_processes)
 
-        try:
-            celery_subprocess.kill()
-        except:
-            pass
-
-        try:
-            file_uploader_subprocess.kill()
-        except:
-            pass
-
-                
-        print "Stopping..."
-        time.sleep(8)
+        for base_port in range(0,num_processes):
+            self._pool.apply_async(teardown_selenium_stack, [base_port, subprocess_stacks[base_port], output_files[base_port]])
+            
+        self._pool.close()
+        self._pool.join()
+        print "Stacks closed."
---
+++ functional_tests/testrunner.py
@@ -0,0 +1,201 @@
+import os
+import sys
+
+import nose
+from nose.config import Config, all_config_files
+from nose.plugins.manager import DefaultPluginManager
+
+from django.core.management.base import BaseCommand
+from django.test import utils
+try:
+    from django.test.simple import DjangoTestSuiteRunner
+except ImportError:
+    from djangosanetesting.runnercompat import DjangoTestSuiteRunner
+
+from djangosanetesting.noseplugins import (
+    DjangoPlugin,
+    DjangoLiveServerPlugin, SeleniumPlugin, CherryPyLiveServerPlugin,
+    DjangoTranslationPlugin,
+    ResultPlugin,
+)
+
+__all__ = ("DstNoseTestSuiteRunner",)
+
+"""
+Act as Django test runner, but use nose. Enable common django-sane-testing
+plugins by default.
+
+You can use
+
+    DST_NOSE_ARGS = ['list', 'of', 'args']
+
+in settings.py for arguments that you always want passed to nose.
+
+Test runners themselves are basically copypasted from django-nose project.
+(C) Jeff Balogh and contributors, released under BSD license.
+
+Thanks and kudos.
+
+Modified for django-sane-testing by Almad.
+"""
+
+def activate_plugin(plugin, argv=None):
+    argv = argv or sys.argv
+    if plugin.activation_parameter not in argv:
+        argv.append(plugin.activation_parameter)
+
+try:
+    any
+except NameError:
+    def any(iterable):
+        for element in iterable:
+            if element:
+                return True
+        return False
+
+OPTION_TRANSLATION = {'--failfast': '-x'}
+
+class DstNoseTestSuiteRunner(DjangoTestSuiteRunner):
+
+    def run_suite(self, nose_argv=None):
+        """Test runner that invokes nose."""
+        # Prepare django for testing.
+        from django.conf import settings
+        utils.setup_test_environment()
+        old_db_name = settings.DATABASE_NAME
+    
+        result_plugin = ResultPlugin()
+        plugins = [DjangoPlugin(), SeleniumPlugin(), DjangoTranslationPlugin(), result_plugin]
+        
+        if getattr(settings, 'CHERRYPY_TEST_SERVER', False):
+            plugins.append(CherryPyLiveServerPlugin())
+        else:
+            plugins.append(DjangoLiveServerPlugin())
+        
+        # Do not pretend it's a production environment.
+        # settings.DEBUG = False
+    
+        # We pass nose a list of arguments that looks like sys.argv, but customize
+        # to avoid unknown django arguments.
+
+        for plugin in _get_plugins_from_settings():
+            plugins_to_add.append(plugin)
+        # activate all required plugins
+        activate_plugin(DjangoPlugin, nose_argv)
+        activate_plugin(SeleniumPlugin, nose_argv)
+        activate_plugin(DjangoTranslationPlugin, nose_argv)
+    #    activate_plugin(ResultPlugin, nose_argv)
+    
+        if getattr(settings, 'CHERRYPY_TEST_SERVER', False):
+            activate_plugin(CherryPyLiveServerPlugin, nose_argv)
+        else:
+            activate_plugin(DjangoLiveServerPlugin, nose_argv)
+    
+        # Skip over 'manage.py test' and any arguments handled by django.
+        django_opts = ['--noinput']
+        for opt in BaseCommand.option_list:
+            django_opts.extend(opt._long_opts)
+            django_opts.extend(opt._short_opts)
+    
+        nose_argv.extend(OPTION_TRANSLATION.get(opt, opt)
+                         for opt in sys.argv[1:]
+                         if opt.startswith('-') and not any(opt.startswith(d) for d in django_opts))
+    
+        if self.verbosity >= 2:
+            print ' '.join(nose_argv)
+    
+        test_program = nose.core.TestProgram(argv=nose_argv, exit=False,
+                                                    addplugins=plugins)
+        
+        # FIXME: ResultPlugin is working not exactly as advertised in django-nose
+        # multiple instance problem, find workaround
+    #    result = result_plugin.result
+    #    return len(result.failures) + len(result.errors)
+        return not test_program.success
+
+    def run_tests(self, test_labels, extra_tests=None):
+        """
+        Run the unit tests for all the test names in the provided list.
+
+        Test names specified may be file or module names, and may optionally
+        indicate the test case to run by separating the module or file name
+        from the test case name with a colon. Filenames may be relative or
+        absolute.  Examples:
+
+        runner.run_tests('test.module')
+        runner.run_tests('another.test:TestCase.test_method')
+        runner.run_tests('a.test:TestCase')
+        runner.run_tests('/path/to/test/file.py:test_function')
+
+        Returns the number of tests that failed.
+        """
+        
+        from django.conf import settings
+        
+        nose_argv = ['nosetests', '--verbosity', str(self.verbosity)] + list(test_labels)
+        if hasattr(settings, 'NOSE_ARGS'):
+            nose_argv.extend(settings.NOSE_ARGS)
+
+        # Skip over 'manage.py test' and any arguments handled by django.
+        django_opts = ['--noinput']
+        for opt in BaseCommand.option_list:
+            django_opts.extend(opt._long_opts)
+            django_opts.extend(opt._short_opts)
+
+        nose_argv.extend(OPTION_TRANSLATION.get(opt, opt)
+                         for opt in sys.argv[1:]
+                         if opt.startswith('-') and not any(opt.startswith(d) for d in django_opts))
+
+        if self.verbosity >= 2:
+            print ' '.join(nose_argv)
+
+        result = self.run_suite(nose_argv)
+        ### FIXME
+        class SimpleResult(object): pass
+        res = SimpleResult()
+        res.failures = ['1'] if result else []
+        res.errors = []
+        # suite_result expects the suite as the first argument.  Fake it.
+        return self.suite_result({}, res)
+
+
+def _get_options():
+    """Return all nose options that don't conflict with django options."""
+    cfg_files = nose.core.all_config_files()
+    manager = nose.core.DefaultPluginManager()
+    config = nose.core.Config(env=os.environ, files=cfg_files, plugins=manager)
+    options = config.getParser().option_list
+    django_opts = [opt.dest for opt in BaseCommand.option_list] + ['version']
+    return tuple(o for o in options if o.dest not in django_opts and
+                                       o.action != 'help')
+
+def _get_plugins_from_settings():
+    from django.conf import settings
+    if hasattr(settings, 'NOSE_PLUGINS'):
+        for plg_path in settings.NOSE_PLUGINS:
+            try:
+                dot = plg_path.rindex('.')
+            except ValueError:
+                raise exceptions.ImproperlyConfigured(
+                                    '%s isn\'t a Nose plugin module' % plg_path)
+            p_mod, p_classname = plg_path[:dot], plg_path[dot+1:]
+            try:
+                mod = import_module(p_mod)
+            except ImportError, e:
+                raise exceptions.ImproperlyConfigured(
+                        'Error importing Nose plugin module %s: "%s"' % (p_mod, e))
+            try:
+                p_class = getattr(mod, p_classname)
+            except AttributeError:
+                raise exceptions.ImproperlyConfigured(
+                        'Nose plugin module "%s" does not define a "%s" class' % (
+                                                                p_mod, p_classname))
+            yield p_class()
+
+# Replace the builtin command options with the merged django/nose options.
+DstNoseTestSuiteRunner.options = _get_options()
+DstNoseTestSuiteRunner.__test__ = False
+
+def run_tests(test_labels, verbosity=1, interactive=True, failfast=False, extra_tests=None):
+    test_runner = DstNoseTestSuiteRunner(verbosity=verbosity, interactive=interactive, failfast=failfast)
+    return test_runner.run_tests(test_labels, extra_tests=extra_tests)
